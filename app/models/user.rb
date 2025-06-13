fd30588693ae194c20746f957c97a7ab6f4a9a87
acm-wq <camisglhdadae@gmail.com>
2025-06-13 17:26:11 +0700

    #9 ☕ Add roles and user_roles with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
   before_save :downcase_email
 
   validates :email,
@@ -15,6 +18,10 @@ class User < ApplicationRecord
   validates :phone,
             format: { with: /\A\+?\d{10,15}\z/, message: 'must be a valid phone number' },
             allow_blank: true
+
+  def admin?
+    roles.exists?(name: 'admin')
+  end
 
   private
 
